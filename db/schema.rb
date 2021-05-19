24f551472ab2aca7caf60d3e297a707a255ef6f2
solar05 <artem00298@gmail.com>
2021-05-20 02:33:28 +0300

    Import bootstrap and generate book resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_19_232600) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "authors"
+    t.string "annotation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
