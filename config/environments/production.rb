c54155c3c65f601d51f84a5e133688be6e226782
solar05 <artem00298@gmail.com>
2021-05-24 04:07:38 +0300

    Add nginx to compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,8 @@ Rails.application.configure do
   # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+  config.assets.compile = true
+  #  config.serve_static_assets = true
+
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
